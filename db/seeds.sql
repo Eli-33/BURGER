d5a06ba028b59ce00f040a4f56473b67bb05d552
Elijam <elijam1608@gmail.com>
2020-11-12 10:51:04 +1100

    add the files & database & all connections

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+ INSERT INTO burgers (burger_name) VALUES ('Chicken burger');
+ INSERT INTO burgers (burger_name) VALUES ('Ham burger');
+ INSERT INTO burgers (burger_name) VALUES ('Cheese burger');
+ INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese burger', true);
+ INSERT INTO burgers (burger_name, devoured) VALUES ('Ham burger', true);
+ INSERT INTO burgers (burger_name) VALUES ('Royal burger');
