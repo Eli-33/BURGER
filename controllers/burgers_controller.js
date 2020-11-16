dd3b9604cd5c3eda3c16874d6542ac0576bc43f0
Elijam <elijam1608@gmail.com>
2020-11-16 18:21:23 +1100

    work on some buges and frontend

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,61 +1,28 @@
-var express = require("express");
-
-var router = express.Router();
-
-// Import the model (burger.js) to use its database functions.
-var burger = require("../models/burger.js");
+const express = require("express");
+const router = express.Router();
+const burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-    burger.all(function(data) {
-    var hbsObject = {
+    burger.selectAll(function(data) {
+    const hbsObject = {
       burgers: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
-
-router.post("/api/burgers", function(req, res) {
-  burger.create([
-    "burger_name", "devoured"
-  ], [
-    req.body.burger_name, req.body.devoured
-  ], function(result) {
-    // Send back the ID of the new quote
+// Create a New Burger
+router.post("/burger/create", function(req, res) {
+  burger.insertOne( req.body.burger_name,function(result) {
     res.json({ id: result.insertId });
   });
 });
-
-router.put("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  console.log("condition", condition);
-
-  burger.update({
-    devoured: req.body.devoured
-  }, condition, function(result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
+// Devour a Burger
+router.post('/burger/eat/:id', function (req, res) {
+  burger.updateOne(req.params.id, function() {
+    res.redirect('/index');
   });
 });
-
-router.delete("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  burger.delete(condition, function(result) {
-    if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
-
 // Export routes for server.js to use.
 module.exports = router;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+// const exphbs = require("express-handlebars");
 
 const PORT = process.env.PORT || 8080;
 
@@ -18,12 +19,10 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-const routes = require("./controllers/catsController.js");
+const routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-  // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
