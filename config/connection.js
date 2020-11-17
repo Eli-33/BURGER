79910bd63702a6b41524cd9b7e9f73366400e9d1
Elijam <elijam1608@gmail.com>
2020-11-17 18:13:49 +1100

    debug

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
+let connection;
 
 // JAWDB connection
 if (process.env.JAWSDB_URL) 
@@ -7,10 +8,10 @@ if (process.env.JAWSDB_URL)
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
+    host: "gx97kbnhgjzh3efb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "w82zrk97vfumxse7",
     password: "js4sz85xm6v8n9a5",
-    database: "burgers_db"
+    database: "isnb9c6vp7hb1tjd"
   });
 
 };
