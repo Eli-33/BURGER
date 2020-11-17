f1bc4bb0c50aff9bda5797728303683ffdefdab2
Eli-33 <elijam1608@gmail.com>
2020-11-17 11:39:51 +1100

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,16 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
-const connection;
 
 if (process.env.JAWSDB_URL) 
 {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
+    host: "gx97kbnhgjzh3efb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: "",
-    database: "burgers_db"
+    user: "w82zrk97vfumxse7",
+    password: "js4sz85xm6v8n9a5",
+    database: "isnb9c6vp7hb1tjd"
   });
 
 };
